544dd427e265bf940b625ebbee85e17a6e19766b
Eric43 <eric.olle@gmail.com>
2019-02-27 15:03:47 -0500

    basic function start and backup

--- source/R/trauma_sim.R
+++ source/R/trauma_sim.R
@@ -10,4 +10,24 @@
 #' of concept.  Later on can write a better rejection sameling method
 #' based upon trauma data.
 #'
+#'
+#' @param n_size is the numbere of simulated pts to be generated
+#' @param p_trauma is a lookup or probability tabl or sample set
+#' @param rej_sample (unused) planed bayesian rejection sampling
+#' @param sim_type is text of "pt", "zip", or "geocode"
+#'
+#'
+#'
 #' 
+trauma_sim <- function(n_size = 2000,
+                       p_trauma = ..., # a look up table of trauma P's
+                       rej_sample = FALSE,
+                       sim_type = "pat")
+{
+                                        # Start with p_trauma table and guesstimate
+
+
+
+    
+
+}
